e8f277b12f92ca3b3cf88fb19799fe8494cfb3d1
PetitChaton <alexandre.t13@gmail.com>
2016-06-02 11:55:59 +0200

    Mise en place

---
+++ js/app.js
@@ -0,0 +1,28 @@
+angular.module("assom_forum", ["ionic","ionMdInput","ionic-material","ionic.rating","ionicLazyLoad","assom_forum.controllers", "assom_forum.services"])
+	.run(function($ionicPlatform) {
+		$ionicPlatform.ready(function() {
+			if(window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
+			}
+
+			if(window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+
+			setTimeout(function() {
+			}, 100);
+
+		});
+	})
+
+
+	.filter("to_trusted", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+
+
+
+
---
+++ js/services.js
@@ -0,0 +1,1 @@
+angular.module("assom_forum.services", [])
